67e513467096607595a44a5014e6254bf68a4c08
Eishal <eishal.cecos@gmail.com>
2025-02-24 19:32:35 +0500

    weather
    weather

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-
+import Weather from './components/Weather'
 const App = () => {
   return (
     <div className='app'>
-      
+      <Weather/>
     </div>
   )
 }
---
+++ src/weather.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Weather.css'
+const weather = () => {
+  return (
+    <div>
+      <h1>Weather</h1>
+    </div>
+  )
+}
+
+export default Weather
