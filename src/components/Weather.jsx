2f8b871a26b27122cf6d3e98fbc14d4513df781d
Eishal <eishal.cecos@gmail.com>
2025-02-25 21:11:02 +0500

    weather
    weather

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import './Weather.css'
-import search_icon from '../components'
+import search_icon from '../components/search.png'
 const Weather = () => {
   return (
     <div className='weather'>
       <div className="search-bar">
         <input type="text" placeholder='Search'/>
-        <img src="" alt="" />
+        <img src={search_icon} alt="" className='search'/>
       </div>
     </div>
   )
